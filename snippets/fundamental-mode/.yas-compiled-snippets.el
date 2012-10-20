022b51bfae352aa6012c92a912df5dd15c5abcae
tadamak <tadamak@gmail.com>
2012-10-21 03:10:14 +0900

    主にruby用のsnippetを設定

---
+++ snippets/fundamental-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `fundamental-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'fundamental-mode
+                     '(("chpasswd-slc" "@echo off\n\nnet user timadmin  $0\nnet user timapp    $1\nnet user timinfra  $2\n\nnet user slcuser   $3\nnet user slocation $4\n\nnet user udx       $5\n\npause" "chpasswd-slc" nil nil nil nil "direct-keybinding" nil)))
+
+
+;;; Do not edit! File generated at Sun Oct 21 01:54:58 2012
