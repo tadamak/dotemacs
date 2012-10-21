705c474537356eba343ff7749f04f98a55a131bd
tadamak <tadamak@gmail.com>
2012-10-21 23:23:40 +0900

    preludeのflyspell起動方法が変わったのに追従

--- personal/init-editor.el
+++ personal/init-editor.el
@@ -2,5 +2,13 @@
 (setq x-select-enable-clipboard t)
 
 ;; turn off flyspell-mode
-(remove-hook 'message-mode-hook 'prelude-turn-on-flyspell)
-(remove-hook 'text-mode-hook 'prelude-turn-on-flyspell)
+(remove-hook 'message-mode-hook 'flyspell-mode)
+(remove-hook 'text-mode-hook 'flyspell-mode)
+
+;; projectile
+(add-to-list 'projectile-globally-ignored-directories "bundle")
+(add-to-list 'projectile-globally-ignored-directories "cache")
+(add-to-list 'projectile-globally-ignored-directories "tmp")
+
+;; for yasnippet
+(setq require-final-newline nil)
