d3d2a8c27cfd4abc86cda3833257d785d194a9ad
tadamak <tadamak@gmail.com>
2012-12-21 12:01:52 +0900

    add some helm keybindings

--- personal/setup-helm.el
+++ personal/setup-helm.el
@@ -1,7 +1,14 @@
 
 (global-set-key (kbd "M-h") 'helm-prelude)
+(global-set-key (kbd "C-x C-c") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x C-i") 'helm-imenu)
+(global-set-key (kbd "C-M-s") 'helm-occur)
+(global-set-key (kbd "C-M-z") 'helm-resume)
+(global-set-key (kbd "C-x C-r") 'helm-recentf)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
 
 ;; helm-c-yasnippet
 (require 'helm-c-yasnippet)
 (setq helm-c-yas-space-match-any-greedy t) ;[default: nil]
-(global-set-key (kbd "C-c y") 'helm-c-yas-complete)
+(global-set-key (kbd "M-=") 'helm-c-yas-complete)
