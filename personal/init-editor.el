a3c44be5474ed2aac9ce2a346e52ff7ad6c53bf8
tadamak <tadamak@gmail.com>
2013-03-05 11:46:02 +0900

    エラーが出る上defaultの8で困らないので設定削除

--- personal/init-editor.el
+++ personal/init-editor.el
@@ -9,9 +9,6 @@
       prelude-flyspell nil
       )
 
-(add-hook 'prelude-prog-mode-hook
-          (lambda () (flycheck-mode -1)))
-
 ;; projectile
 (add-to-list 'projectile-globally-ignored-directories "vendor")
 (add-to-list 'projectile-globally-ignored-directories "cache")
@@ -27,17 +24,23 @@
 (setq cua-enable-cua-keys nil) ; 不都合なキーバインドを無効化
 
 ;; markdown
+;; from http://support.markedapp.com/kb/how-to-tips-and-tricks/marked-bonus-pack-scripts-commands-and-bundles
+(defun markdown-preview-file ()
+  "run Marked on the current file and revert the buffer"
+  (interactive)
+  (shell-command (format "open -a /Applications/Marked.app %s" (shell-quote-argument (buffer-file-name)))))
+
 (add-hook 'markdown-mode-hook
   (lambda()
     (define-key markdown-mode-map (kbd "C-i") 'markdown-cycle)
-    (hide-sublevels 2)))
+    (hide-sublevels 2)
+    (define-key markdown-mode-map (kbd "C-c m") 'markdown-preview-file)))
 
 ;; coffee-mode
 (autoload 'coffee-mode "coffee-mode" "Major mode for editing CoffeeScript." t)
 (add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
 (add-hook 'coffee-mode-hook
   (lambda()
-    (setq (make-local-variable 'tab-width) 2)
     (setq coffee-tab-width 2)
     (setq whitespace-action '(auto-cleanup)) ;; automatically clean up bad whitespace
     (setq whitespace-style '(trailing space-before-tab indentation empty space-after-tab)) ;; only show bad whitespace
@@ -46,3 +49,11 @@
 ;; flymake
 (set-face-foreground 'flymake-errline "tomato")
 (set-face-background 'flymake-errline nil)
+
+;; yagist
+(when (require 'yagist nil t)
+  (setq yagist-encrypt-risky-config t)
+  (setq yagist-authenticate-function 'yagist-oauth2-authentication)
+  (define-key yagist-list-mode-map "g" nil)
+  (setq revert-buffer-function 'revert-buffer) ;; 他modeのbufferであってもyagistのrevert-bufferに取られてしまう
+  )
