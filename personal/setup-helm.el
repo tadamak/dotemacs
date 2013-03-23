5f73b93775209440dffd363190afd3c6de63f803
tadamak <tadamak@gmail.com>
2013-03-23 21:11:11 +0900

    helm: ctags

--- personal/setup-helm.el
+++ personal/setup-helm.el
@@ -1,4 +1,3 @@
-
 (global-set-key (kbd "M-h") 'helm-prelude)
 (global-set-key (kbd "C-x C-c") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
@@ -7,6 +6,7 @@
 (global-set-key (kbd "C-M-z") 'helm-resume)
 (global-set-key (kbd "C-x C-r") 'helm-recentf)
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
+(global-set-key (kbd "M-.") 'helm-c-etags-select)
 
 ;; helm-c-yasnippet
 (require 'helm-c-yasnippet)
