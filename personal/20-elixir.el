e55793ee730f15df820352f00065631d4585b22e
Takehiro KAMADA <tadamak@timedia.co.jp>
2016-06-28 03:40:25 +0900

    elixir-mode: auto insert `end`

--- personal/20-elixir.el
+++ personal/20-elixir.el
@@ -5,12 +5,25 @@
        (elixir-cos-mode t)))
 
 (when (require 'elixir-mode nil t)
-  ;(add-hook 'elixir-mode-hook 'elixir-mode-compile-on-save)
-  (add-to-list 'elixir-mode-hook
-               (defun auto-activate-ruby-end-mode-for-elixir-mode ()
-                 (set (make-variable-buffer-local 'ruby-end-expand-keywords-before-re)
-                      "\\(?:^\\|\\s-+\\)\\(?:do\\)")
-                 (set (make-variable-buffer-local 'ruby-end-check-statement-modifiers) nil)
-                 (ruby-end-mode +1)))
+  ;; (add-hook 'elixir-mode-hook 'elixir-mode-compile-on-save)
+  ;; (add-to-list 'elixir-mode-hook
+  ;;              (defun auto-activate-ruby-end-mode-for-elixir-mode ()
+  ;;                (set (make-variable-buffer-local 'ruby-end-expand-keywords-before-re)
+  ;;                     "\\(?:^\\|\\s-+\\)\\(?:do\\)")
+  ;;                (set (make-variable-buffer-local 'ruby-end-check-statement-modifiers) nil)
+  ;;                (ruby-end-mode +1)))
   )
+
+(defun my-elixir-do-end-close-action (id action context)
+  (when (eq action 'insert)
+    (newline-and-indent)
+    (previous-line)
+    (indent-according-to-mode)))
+
+(sp-with-modes '(elixir-mode)
+  (sp-local-pair "do" "end"
+                 :when '(("SPC" "RET"))
+                 :post-handlers '(:add my-elixir-do-end-close-action)
+                 :actions '(insert)))
+
 (when (require 'alchemist nil t))
